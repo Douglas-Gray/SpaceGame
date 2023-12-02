a147b36a6c0268449303bf4fcb90ded3e67b7616
Douglas <douglasgray155@gmail.com>
2023-12-02 17:38:05 +0000

    Added game manage to handle game content

---
+++ SpaceGame/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using SpaceGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame.Managers
+{
+    public class GameManager
+    {
+
+        private readonly Player _player; 
+        public GameManager()
+        {
+
+            _player = new(Globals.Content.Load<Texture2D>("player"), new(50, 50)); 
+
+        }
+
+        public void Update()
+        {
+            InputManager.Update(); 
+            _player.Update();
+        }
+
+        public void Draw()
+        {
+            _player.Draw(); 
+        }
+ 
+    }
+}
