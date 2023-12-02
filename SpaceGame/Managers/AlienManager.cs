ec58acdddffef23dea343285a188aea188d6c98d
Douglas <douglasgray155@gmail.com>
2023-12-02 19:15:21 +0000

    Added logc to spawn in and handle enemies within game

---
+++ SpaceGame/Managers/AlienManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using SpaceGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame.Managers
+{
+    public static class AlienManager
+    {
+        public static List<Alien> Aliens { get; } = new();
+        private static Texture2D _texture; 
+
+        public static void Init()
+        {
+            _texture = Globals.Content.Load<Texture2D>("AlienGreen"); 
+        }
+
+        public static void AddAlien()
+        {
+            Aliens.Add(new(_texture, new(100, 100)) ); 
+        }
+
+        public static void Update(Player player)
+        { 
+            foreach (var a in Aliens)
+            {
+                a.Update(player); 
+            }
+        }
+
+        public static void Draw()
+        {
+            foreach (var a in Aliens)
+            {
+                a.Draw(); 
+            }
+        }
+    }
+}
---
+++ SpaceGame/Models/Alien.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame.Models
+{
+    public class Alien : Sprite
+    {
+        public Alien(Texture2D texture, Vector2 position) : base(texture, position)
+        {
+            Speed = 100; 
+        }
+
+        public void Update(Player player)
+        {
+            var toPlayer = player.Position - Position;
+            Rotation = (float)Math.Atan2(toPlayer.Y, toPlayer.X); 
+
+            if (toPlayer.Length() > 4)
+            {
+                var dir = Vector2.Normalize(toPlayer);
+                Position += dir * Speed * Globals.TotalSeconds;
+            }
+        }
+    }
+}
