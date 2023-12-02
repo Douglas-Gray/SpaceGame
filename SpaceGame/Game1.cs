c8d27f1819264d03b2daa6c388147dba97761dee
Douglas <douglasgray155@gmail.com>
2023-12-02 16:52:39 +0000

    inital commit

---
+++ SpaceGame/Game1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D playerTexture;
+        Vector2 playerPosition;
+        float playerSpeed; 
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            //Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            playerPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+            _graphics.PreferredBackBufferHeight / 2);
+            playerSpeed = 100f;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            playerTexture = Content.Load<Texture2D>("player");
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(
+            playerTexture,
+            playerPosition,
+            null,
+            Color.White,
+            0f,
+            new Vector2(playerTexture.Width / 2, playerTexture.Height / 2),
+            Vector2.One,
+            SpriteEffects.None,
+            0f
+            );
+
+            _spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
