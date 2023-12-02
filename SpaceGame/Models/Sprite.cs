6a0aaf7c13f8ce5eba323a8022c7583f4fb5a03b
Douglas <douglasgray155@gmail.com>
2023-12-02 17:38:50 +0000

    Added Sprite Class to handle sprite positions and drawing

---
+++ SpaceGame/Models/Sprite.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame.Models
+{
+    public class Sprite
+    {
+        protected readonly Texture2D texture;
+        protected readonly Vector2 origin;
+        public Vector2 Position { get; set; }
+        public int Speed { get; set; }
+        public float Rotation { get; set; }
+
+        public Sprite (Texture2D spriteTexture, Vector2 spritePosition)
+        {
+            texture = spriteTexture;
+            Position = spritePosition;
+            Speed = 300;
+            origin = new(spriteTexture.Width / 2, spriteTexture.Height / 2); 
+        }
+
+        public virtual void Draw()
+        {
+            Globals.SpriteBatch.Draw(texture, Position, null, Color.White, Rotation, origin, 1, SpriteEffects.None, 1); 
+        }
+
+    }
+}
